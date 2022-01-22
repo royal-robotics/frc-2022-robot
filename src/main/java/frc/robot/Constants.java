67a9ff3efc7056ef2dc8ff9fc22b57af499bf204
Royal Robotics <royalrobotics@outlook.com>
2022-01-22 13:42:45 -0800

    added swerve drive command

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,33 @@
+package frc.robot;
+
+public class Constants {
+    public static final int DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR = 2;
+    public static final int DRIVETRAIN_FRONT_RIGHT_DRIVE_MOTOR = 0;
+    public static final int DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR = 3;
+    public static final int DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR = 1;
+
+    public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR = 6;
+    public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_MOTOR = 5;
+    public static final int DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR = 4;
+    public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR = 7;
+
+    public static final int DRIVETRAIN_FRONT_LEFT_ENCODER_PORT = 3;
+    public static final int DRIVETRAIN_FRONT_RIGHT_ENCODER_PORT = 1;
+    public static final int DRIVETRAIN_BACK_LEFT_ENCODER_PORT = 0;
+    public static final int DRIVETRAIN_BACK_RIGHT_ENCODER_PORT = 2;
+
+    public static final int PIGEON_PORT = 0;
+
+    // In degrees
+    //public static final double DRIVETRAIN_FRONT_LEFT_ENCODER_OFFSET = Math.toRadians(65.89495849609318 - 52.944824218748884);
+    //public static final double DRIVETRAIN_FRONT_RIGHT_ENCODER_OFFSET = Math.toRadians(340.0961303710937 - 297.8811645507811);
+    //public static final double DRIVETRAIN_BACK_LEFT_ENCODER_OFFSET = Math.toRadians(219.12030029296875 - 201.41857910156259);
+    //public static final double DRIVETRAIN_BACK_RIGHT_ENCODER_OFFSET = Math.toRadians(30 + 183.38239746093726 - 153.22492675781223);
+
+    public static final double DRIVETRAIN_FRONT_LEFT_ENCODER_OFFSET = Math.toRadians(0);
+    public static final double DRIVETRAIN_FRONT_RIGHT_ENCODER_OFFSET = Math.toRadians(0);
+    public static final double DRIVETRAIN_BACK_LEFT_ENCODER_OFFSET = Math.toRadians(0);
+    public static final double DRIVETRAIN_BACK_RIGHT_ENCODER_OFFSET = Math.toRadians(0);
+
+    public static final int PRIMARY_CONTROLLER_PORT = 0;
+}
